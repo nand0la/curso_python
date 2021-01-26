bee89357187393738df88ef285804e79ccdea186
Fernando Santos <santos.fluiz112@gmail.com>
2021-01-26 12:49:43 -0300

    começando projeto do zero

--- 9_projeto_agenda/contatos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Contato
-
-class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'categoria')
-    list_display_links = ('id', 'nome')
-
-admin.site.register(Categoria)
-admin.site.register(Contato, ContatoAdmin)
